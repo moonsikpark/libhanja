4c08dd4fd13c70711962953c3bbbc65e9a39bc0d
Moonsik Park <47686265+moonsikpark@users.noreply.github.com>
2023-03-08 12:09:31 +0900

    feat: add `__repr__` to all exposed values

--- src/pyhanja.cc
+++ src/pyhanja.cc
@@ -1,4 +1,5 @@
 // Copyright (c) 2022 ESTsoft Corp. All rights reserved.
+#include <string>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
@@ -14,13 +15,23 @@ PYBIND11_MODULE(_pyhanja, m) {
   py::class_<hanja::dictionary::DictionaryItem>(m, "DictionaryItem")
       .def_property_readonly("key", &hanja::dictionary::DictionaryItem::get_key)
       .def_property_readonly("value",
-                             &hanja::dictionary::DictionaryItem::get_value);
+                             &hanja::dictionary::DictionaryItem::get_value)
+      .def("__repr__",
+          [](const hanja::dictionary::DictionaryItem &i) {
+              return "<pyhanja.DictionaryItem key=" + i.get_key() + " value=" + i.get_value() + ">";
+          }
+      );
 
   py::class_<hanja::dictionary::Dictionary>(m, "Dictionary")
       .def(py::init<>())
       .def("add_data", &hanja::dictionary::Dictionary::add_data)
       .def("query", &hanja::dictionary::Dictionary::query)
-      .def_property_readonly("size", &hanja::dictionary::Dictionary::size);
+      .def_property_readonly("size", &hanja::dictionary::Dictionary::size)
+      .def("__repr__",
+          [](const hanja::dictionary::Dictionary &i) {
+              return "<pyhanja.Dictionary size=" + std::to_string(i.size()) + ">";
+          }
+      );
 
   py::class_<hanja::convert::Convert>(m, "Convert")
       .def(py::init<const hanja::compat::string &,
@@ -34,7 +45,12 @@ PYBIND11_MODULE(_pyhanja, m) {
   py::class_<hanja::types::MatchPosition>(m, "MatchPosition")
       .def_property_readonly("pos", &hanja::types::MatchPosition::get_pos)
       .def_property_readonly("key", &hanja::types::MatchPosition::get_key)
-      .def_property_readonly("value", &hanja::types::MatchPosition::get_value);
+      .def_property_readonly("value", &hanja::types::MatchPosition::get_value)
+      .def("__repr__",
+          [](const hanja::types::MatchPosition &i) {
+              return "<pyhanja.MatchPosition pos=" + std::to_string(i.get_pos()) + " key='" + i.get_key() + "' value=" + i.get_value() + "'>";
+          }
+      );
 
 #ifdef VERSION_INFO
   m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
